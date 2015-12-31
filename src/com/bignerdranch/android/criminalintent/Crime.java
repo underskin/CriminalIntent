d4ac1a97e4ece84b6e79bcf0e0a7498b9a88a2a4
underskin <underskin@163.com>
2015-12-31 22:34:48 +0800

    PC Ch 21.2

--- src/com/bignerdranch/android/criminalintent/Crime.java
+++ src/com/bignerdranch/android/criminalintent/Crime.java
@@ -13,12 +13,14 @@ public class Crime {
 	private static final String JSON_SOLVED = "solved";
 	private static final String JSON_DATE = "date";
 	private static final String JSON_PHOTO = "photo";
+	private static final String JSON_SUSPECT = "suspect";
 
 	private UUID mId;
 	private String mTitle;
 	private Date mDate;
 	private boolean mSolved;
 	private Photo mPhoto;
+	private String mSuspect;
 
 	public Crime() {
 		mId = UUID.randomUUID();
@@ -33,6 +35,8 @@ public class Crime {
 		if (json.has(JSON_PHOTO)) {
 			mPhoto = new Photo(json.getJSONObject(JSON_PHOTO));
 		}
+		if (json.has(JSON_SUSPECT))
+			mSuspect = json.getString(JSON_SUSPECT);
 	}
 
 	public JSONObject toJSON() throws JSONException {
@@ -44,6 +48,7 @@ public class Crime {
 		if (mPhoto != null) {
 			json.put(JSON_PHOTO, mPhoto.toJSON());
 		}
+		json.put(JSON_SUSPECT, mSuspect);
 		return json;
 	}
 
@@ -86,5 +91,13 @@ public class Crime {
 
 	public void setPhoto(Photo p) {
 		mPhoto = p;
+	}
+
+	public String getSuspect() {
+		return mSuspect;
+	}
+
+	public void setSuspect(String suspect) {
+		mSuspect = suspect;
 	}
 }
